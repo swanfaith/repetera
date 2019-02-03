9595b89f429998c45c98fc6ee5acf302aa75802e
Leila Svantro <leila.svantro+bitbucket@gmail.com>
2019-02-03 16:07:20 +0100

    Initial commit

---
+++ src/main/java/io/github/swanfaith/repetera/RepeteraApplication.java
@@ -0,0 +1,14 @@
+package io.github.swanfaith.repetera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepeteraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepeteraApplication.class, args);
+	}
+
+}
+
